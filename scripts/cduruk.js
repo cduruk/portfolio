e6dd6bde3633e543b03d9fc42fee49257e229ed5
Can Duruk <can@duruk.net>
2010-01-25 23:11:35 -0500

    first commit

---
+++ scripts/cduruk.js
@@ -0,0 +1,9 @@
+// Make the new HTML5 tags work in IE
+<script>
+  document.createElement('header');
+  document.createElement('footer');
+  document.createElement('section');
+  document.createElement('aside');
+  document.createElement('nav');
+  document.createElement('article');
+</script>
