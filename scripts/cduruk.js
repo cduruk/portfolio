2e63b9128554cb7e3a78bfd40f9aa6a87175ec03
Can Duruk <can@duruk.net>
2010-01-28 13:56:14 -0500

    Forgot to put this

--- scripts/cduruk.js
+++ scripts/cduruk.js
@@ -1,9 +1,7 @@
 // Make the new HTML5 tags work in IE
-<script>
   document.createElement('header');
   document.createElement('footer');
   document.createElement('section');
   document.createElement('aside');
   document.createElement('nav');
   document.createElement('article');
-</script>
