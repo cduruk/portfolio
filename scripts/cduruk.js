2bc64ae6dd3e578b355e519e8bff915333905d27
Can Duruk <can@duruk.net>
2010-01-30 13:53:01 -0500

    Intendation

--- scripts/cduruk.js
+++ scripts/cduruk.js
@@ -1,7 +1,7 @@
 // Make the new HTML5 tags work in IE
-  document.createElement('header');
-  document.createElement('footer');
-  document.createElement('section');
-  document.createElement('aside');
-  document.createElement('nav');
-  document.createElement('article');
+document.createElement('header');
+document.createElement('footer');
+document.createElement('section');
+document.createElement('aside');
+document.createElement('nav');
+document.createElement('article');
