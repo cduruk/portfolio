abaadb123409aaded41c8e8bf7a5ef2da6f63add
Can Duruk <can@duruk.net>
2010-01-30 20:32:41 -0500

    New deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,9 +6,14 @@ set :repository,  "git@github.com:cduruk/portfolio.git"
 role :web, "linode"                          # Your HTTP server, Apache/etc
 role :app, "linode"                          # This may be the same as your `Web` server
 
-set :deploy_to, "/srv/www/beta.duruk.net/public_html"
 set :deploy_via, :export
 set :shared_children, []
+
+if ENV["DURUK_ENV"] == 'beta'
+  set :deploy_to, "/srv/www/beta.duruk.net/public_html"
+else
+  set :deploy_to, "/srv/www/duruk.net/public_html"
+end
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
